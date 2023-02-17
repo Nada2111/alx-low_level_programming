643ec0dffd8dae445ac18d0401f3661ad90327bf
nada <nada151adel@gmail.com>
2023-02-16 17:00:30 -0800

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-* main prints exactly with proper grammar, but the outcome is a piece of art,
+* main - prints exactly with proper grammar, but the outcome is a piece of art,
 * Return: Always 0 (Success)
 */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
-}	
+}
