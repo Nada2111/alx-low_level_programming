41ece1efaeb34ec843b21c69ca306a9aec201120
nada <nada151adel@gmail.com>
2023-02-16 17:36:46 -0800

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
+* main - Entry point You should produce the exact same output as in the example
 * prints the size of various types on the computer it is compiled and run on
 * Return: Always 0 (Success)
 */
