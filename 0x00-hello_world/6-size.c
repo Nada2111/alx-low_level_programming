203ab92db2f96363c08ab08faae1fd3499f9f31d
nada <nada151adel@gmail.com>
2023-02-16 17:34:17 -0800

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
+
 /**
 * main - Entry point
 * prints the size of various types on the computer it is compiled and run on
 * Return: Always 0 (Success)
 */
+
+
+
+
 int main(void)
 {
 	printf("Size of a char: %zu bytes\n", sizeof(char));
