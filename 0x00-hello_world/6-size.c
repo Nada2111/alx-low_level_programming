3bcd58270e008de12fcd985ad540a03f424121a2
nada <nada151adel@gmail.com>
2023-02-16 17:17:28 -0800

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+* prints the size of various types on the computer it is compiled and run on
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	printf("Size of int: %zu bytes\n", sizeof(intType));
-    printf("Size of float: %zu bytes\n", sizeof(floatType));
-    printf("Size of double: %zu bytes\n", sizeof(doubleType));
-    printf("Size of char: %zu byte\n", sizeof(charType));
-
+	printf("Size of char: %zu bytes\n", sizeof(char));
+    printf("Size of int: %zu bytes\n", sizeof(int));
+    printf("Size of long int: %zu bytes\n", sizeof(long int));
+    printf("Size of long long int: %zu byte\n", sizeof(long long int));
+    printf("Size of float: %zu bytes\n", sizeof(float));
     return (0);
 }
