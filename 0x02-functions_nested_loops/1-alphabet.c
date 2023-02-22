31897acf8b9e290096114f3bd1c267fe7076ec3e
nada <nada151adel@gmail.com>
2023-02-22 13:53:01 -0800

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,10 @@
 #include "main.h"
-int main ()
+int main (void)
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
 {
 	char c;
 	for (c ='a' ; c <='z' ; c++)
