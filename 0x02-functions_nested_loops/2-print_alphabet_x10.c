8579fe0252f28a9ee5e91274bf77b024c319e5d6
nada <nada151adel@gmail.com>
2023-02-22 16:47:57 -0800

    task3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,7 +6,7 @@
 void print_alphabet_x10(void)
 {
 char c;
-int i = 0;      
+int i = 0; 
 while (i <= 9)
 {
 for (c = 'a'; c <= 'z'; c++)
@@ -16,4 +16,4 @@ _putchar(c);
 _putchar('\n');
 i++;
 }
-}	
+}
