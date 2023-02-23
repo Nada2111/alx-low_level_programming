35486f048ca7abf7ea1d3711f0310b707394cb78
nada <nada151adel@gmail.com>
2023-02-22 16:02:01 -0800

    task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* print_alphabet - that prints the alphabet, in lowercase, followed by a new line
+* print_alphabet - that prints the alphabet, in lowerce
 */
 void print_alphabet(void)
 {
