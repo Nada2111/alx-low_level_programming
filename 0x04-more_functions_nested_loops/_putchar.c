84ebbd5cb55cdf030032e76fe3e576e2bf0e0a19
nada <nada151adel@gmail.com>
2023-02-23 13:41:48 -0800

    task0

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,15 +1,13 @@
-#include <main.h>
-
+#include "main.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
-*Returns 1 if c is uppercase
-*Returns 0 otherwise
-*/
+ * _isupper - a function that checks for uppercase character
+ * @c: input
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-	return (1);
-	else 
-	return (0);
+	if ((c >= 'A') && (c <= 'Z'))
+		return (1);
+	else
+		return (0);
 }
