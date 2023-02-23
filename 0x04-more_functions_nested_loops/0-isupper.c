d8556a1050161f81ca62e98a71612e423a586adc
nada <nada151adel@gmail.com>
2023-02-23 13:09:34 -0800

    task0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+ /**
+     *  _isupper - function that checks for uppercase character
+     *  int @c
+*/
+ int _isupper(int c)
+{
+    if (c >= 65 && c <= 90)
+    return (1);
+    else
+    return (0);
+}
