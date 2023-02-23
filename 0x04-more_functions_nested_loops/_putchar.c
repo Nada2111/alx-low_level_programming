21bced4749ec28b8abb85079e26a2952edafae9e
nada <nada151adel@gmail.com>
2023-02-23 13:28:23 -0800

    task0

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -3,12 +3,13 @@
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
+*Returns 1 if c is uppercase
+*Returns 0 otherwise
+*/
+int _isupper(int c)
 {
-	return (write(1, &c, 1));
+	if (c >= 'A' && c <= 'Z')
+	return (1);
+	else 
+	return (0);
 }
-
