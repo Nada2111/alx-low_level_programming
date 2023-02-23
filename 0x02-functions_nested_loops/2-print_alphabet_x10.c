a13cc5c8b097833036779d5089452999b3e69d9a
nada <nada151adel@gmail.com>
2023-02-22 16:44:49 -0800

    task3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,10 @@
 #include "main.h"
-/** 
+/**
 * print_alphabet_x10 - prints 10 times the alphabet
 *Return: void
 */
 void print_alphabet_x10(void)
-{ 
+{
 char c;
 int i = 0;      
 while (i <= 9)
