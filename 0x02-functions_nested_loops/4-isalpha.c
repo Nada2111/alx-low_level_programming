d81828c484fa0124c4dd28ae6cd7ae2211de8579
nada <nada151adel@gmail.com>
2023-02-22 17:30:45 -0800

    task4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,13 +2,13 @@
 /**
  * _isalpha - function that checks for alphabetic character
  *
- * Return: 1 when it is C c 
+ * Return: 1 when it is C c
  * else return 0
  */
 int _isalpha(int c)
 {
 	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 		return (1);
-	else 
+	else
 		return (0);
 }
