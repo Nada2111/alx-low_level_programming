17e7ddbf5d648d338ed57d04ccc4a86a8850d5f7
nada <nada151adel@gmail.com>
2023-02-24 17:18:51 -0800

    task2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,19 +14,19 @@ int largest;
 
 if (a > b && a > c)
 {
-     largest = a;
+       largest = a;
 }
 else if (b > a && b > c)
 {
-     largest = b;
+        largest = b;
 }
-else if (c > b && c > a)
+else if (c > b)
 {
 	largest = c;
 }
 else
 {
-    largest = b;
+        largest = b;
 }
 
 return (largest);
