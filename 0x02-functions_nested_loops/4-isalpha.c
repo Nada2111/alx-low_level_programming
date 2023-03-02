c6568bc455f6a8a0a6bf813aae5e52e273a7a177
nada <nada151adel@gmail.com>
2023-03-01 16:09:28 -0800

    task4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,8 +6,16 @@
  */
 int _isalpha(int c)
 {
-	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
-		return (1);
-	else
-		return (0);
+	if( c >= 'a' && c <= 'z')
+	{
+		return (1)
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1)
+	}
+	else 
+	{
+		return (0)
+	}
 }
