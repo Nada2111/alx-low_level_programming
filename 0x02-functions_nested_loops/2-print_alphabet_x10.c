9b5a12bba7d51ca762d06623ecb4d13d5d311cfe
nada <nada151adel@gmail.com>
2023-03-28 22:31:19 -0700

    Task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,6 +2,7 @@
 
 /**
  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ *@i to print 10 time
  * followed by a new line
  */
 void print_alphabet_x10(void)
@@ -9,15 +10,12 @@ void print_alphabet_x10(void)
 	char c;
 	int i = 0;
 
-	while (i < 10)
+	for (c = 'a' ; c <= 'z' ; c++)
 	{
-		c = 'a';
-		while (c <= 'z')
+		for (i = 0 ; i < 10 ; i++)
 		{
 			_putchar(c);
-			c++;
 		}
 		_putchar('\n');
-		i++;
 	}
 }
