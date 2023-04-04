64e6f3ffe56ef08a969b13f2d14e02955229221c
nada <nada151adel@gmail.com>
2023-04-04 15:07:27 -0700

    0

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,7 @@
-#ifndef LISTS_H
-#define LISTS_H
+ #ifndef LISTS_H
+ #define LISTS_H
+ #include <stdio.h>
+ #include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -7,12 +9,6 @@
  * @next: points to next code 
  * Description: singly linked list 
  */
- typedef struct listint_s
-{
-	int n;
-	struct listint_s *next;
+ size_t print_listint(const listint_t *h);
 
-} listint_t
-size_t print_listint(const listint_t *h);
-
-#endif
+ #endif
