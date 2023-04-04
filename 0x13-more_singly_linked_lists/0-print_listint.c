b4c1df6920d5cfa570a3434f04c47824aaaa5729
nada <nada151adel@gmail.com>
2023-04-04 15:41:08 -0700

    task0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,21 +3,21 @@
 
 /**
  * print_listint - that prints all the elements of a listint_t list
- * @h: head of list.
+ * @h : head of list.
  *
  * return: the numbers of nodes
  */
 
 size_t print_listint(const listint_t *h)
+
 {
 	size_t n = 0;
 
-	while (h != NULL)
-		{
-			printf("%d\n" , h->n);
-			n++;
-			h = h->next;
-
-		}
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		n++;
+	}
 	return (n);
 }
