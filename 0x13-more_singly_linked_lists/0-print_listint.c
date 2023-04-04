0178f660670c734a9a0d32c928274d7f6e85eeb9
nada <nada151adel@gmail.com>
2023-04-04 15:43:07 -0700

    task0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,7 +5,7 @@
  * print_listint - that prints all the elements of a listint_t list
  * @h : head of list.
  *
- * return: the numbers of nodes
+ * Return: the numbers of nodes.
  */
 
 size_t print_listint(const listint_t *h)
