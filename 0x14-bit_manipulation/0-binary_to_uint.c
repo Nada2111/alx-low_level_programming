b9e1db4ebffd4ffe2640b7eb4113a47afed9a92f
nada <nada151adel@gmail.com>
2023-04-10 14:36:22 -0700

    task0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * binary_to_uint - convert binary number to unsigned integer number.
+ * @b: char string
+ * Return: converted decimal number or 0 if there is an unconvertable char
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i, p;
+	int f;
+
+	if (b == NULL)
+		return (0);
+
+	for (f = 0; b[f]; f++)
+	{
+		if (b[f] != '0' && b[f] != '1')
+			return (0);
+	}
+
+	for (p = 1, i = 0, f--; f >= 0; f--, p *= 2)
+	{
+		if (b[f] == '1')
+			i += p
+	}
+return (i);
+}
