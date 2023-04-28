7c3737452d43bfda033761f03be4afafd6550fee
nada <nada151adel@gmail.com>
2023-04-28 05:34:43 -0700

    try

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,9 +21,13 @@ int create_file(const char *filename, char *text_content)
 	if (text_content)
 	{
 		while (text_content[i] != '\0')
-			words++;
+			w++;
 
 		w = write(f, text_content, i);
 		if (w == -1)
 			return (-1);
 	}
+
+	close(f);
+	return (1);
+}
