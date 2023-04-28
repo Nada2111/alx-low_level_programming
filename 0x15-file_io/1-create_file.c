840e71552f48fdf6e33515c3ae0b5131e4bfc467
nada <nada151adel@gmail.com>
2023-04-28 05:47:08 -0700

    tasl

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,29 +5,25 @@
  * @filename:  is the name of the file to create
  * @text_content: terminated string to write to the file
  * Return: filename is NULL return -1
-
  **/
 int create_file(const char *filename, char *text_content)
 {
-	int f, w, i = 0;
+	int ff;
+	int ll = 0;
+	int ww = 0;
 
 	if (filename == NULL)
 		return (-1);
-
-	f = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (f == -1)
+	ff = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (ff == -1)
 		return (-1);
-
-	if (text_content)
-	{
-		while (text_content[i] != '\0')
-			w++;
-
-		w = write(f, text_content, i);
-		if (w == -1)
-			return (-1);
-	}
-
-	close(f);
+	if (!text_content)
+		text_content = "";
+	while (text_content[ll] != '\0')
+		ll++;
+	wrw = write(fd, text_content, letters);
+	if (ww == -1)
+		return (-1);
+	close(ff);
 	return (1);
 }
