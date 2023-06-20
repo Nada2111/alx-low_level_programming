b23bbf8e27c038a249ce882b174de741fd43e845
Nada2111 <Nada151adel@gmail.com>
2023-06-19 18:05:36 -0700

    Tasks

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _abs - a function that computes the absolute value of an integer
+ * @i: integer input
+ * Return: absolute value of ab
+ */
+int _abs(int i)
+{
+	return i * ((i > 0) - (i < 0));
+}
